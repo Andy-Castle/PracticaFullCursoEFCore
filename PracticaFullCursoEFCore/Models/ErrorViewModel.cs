5c3be672e4b3d5de693b6cc5ecabb6f3e9afe895
Andy Castle <masterchief_and@live.com>
2025-01-14 14:32:14 -0500

    Add project files.

---
+++ PracticaFullCursoEFCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticaFullCursoEFCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
